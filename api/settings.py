cc5b96877bf8964cc81dd07e0cdccddf811a5a1c
jaxyt <jax.maximillion.taylor@gmail.com>
2020-09-14 21:18:21 +0000

    basic server

--- api/settings.py
+++ api/settings.py
@@ -72,11 +72,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# engine - django.db.backends.sqlite3 name - os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'acedb',
     }
 }
 
